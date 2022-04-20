1a69304a97b3cdc2335778bd72c057f374e0cae8
prec1001udes <102160292+prec1001udes@users.noreply.github.com>
2022-04-20 15:36:42 -0400

    Update _targets.R

--- _targets/_targets.R
+++ _targets/_targets.R
@@ -2,13 +2,23 @@ library(targets)
 library(tarchetypes)
 tar_option_set(packages = c("rmarkdown",
                             "knitr"))
+
 source("functions.R")
-source("R/figure.R")
 
 list(
-    tar_target(
-      table_collab, table_collab_f())
-   )
+  tar_target(
+    table_collab, table_collab_f()),
+  tar_target(
+    table_noeuds, table_noeuds_f()),
+  tar_target(
+    table_cours, table_cours_f()),
+  tar_target(
+    table_clean_collab, doublon_free_collab(table_collab)),
+  tar_target(
+    table_clean_noeuds, doublon_free_noeuds(table_noeuds)),
+  tar_target(
+    table_clean_cours, doublon_free_cours(table_cours))
+)
 
 
               
