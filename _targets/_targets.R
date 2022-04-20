6feaba837760da6506cced2a3b1f275647a895b9
prec1001udes <102160292+prec1001udes@users.noreply.github.com>
2022-04-20 18:14:54 -0400

    Update _targets.R

--- _targets/_targets.R
+++ _targets/_targets.R
@@ -18,6 +18,7 @@ list(
     table_clean_noeuds, doublon_free_noeuds(table_noeuds)),
   tar_target(
     table_clean_cours, doublon_free_cours(table_cours)),
+  #Le sql fonctionne pas pour l'instant
   tar_target(
     table_collab_sql, table_collab_sql_f(table_collab)),
   tar_target(
