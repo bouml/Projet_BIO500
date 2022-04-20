88aa8a9f97c0ac05f0f855dd151606761c60cf03
prec1001udes <102160292+prec1001udes@users.noreply.github.com>
2022-04-20 17:55:34 -0400

    Update _targets.R

--- _targets/_targets.R
+++ _targets/_targets.R
@@ -17,7 +17,13 @@ list(
   tar_target(
     table_clean_noeuds, doublon_free_noeuds(table_noeuds)),
   tar_target(
-    table_clean_cours, doublon_free_cours(table_cours))
+    table_clean_cours, doublon_free_cours(table_cours)),
+  tar_target(
+    table_collab_sql, table_collab_sql_f(table_collab)),
+  tar_target(
+    table_noeuds_sql, table_noeuds_sql_f(table_noeuds)),
+  tar_target(
+    table_cours_sql, table_cours_sql_f(table_cours))
 )
 
 
