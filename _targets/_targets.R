31d483a60a71049915e71cddce4fa3a4f69bfcfd
prec1001udes <102160292+prec1001udes@users.noreply.github.com>
2022-04-21 08:18:03 -0400

    Update _targets.R

--- _targets/_targets.R
+++ _targets/_targets.R
@@ -18,18 +18,31 @@ list(
     table_clean_noeuds, doublon_free_noeuds(table_noeuds)),
   tar_target(
     table_clean_cours, doublon_free_cours(table_cours)),
-  #Le sql fonctionne pas pour l'instant
   tar_target(
-    table_collab_sql, table_collab_sql_f(table_collab)),
+    table_collab_sql, table_collab_sql_f(table_clean_collab)),
   tar_target(
-    table_noeuds_sql, table_noeuds_sql_f(table_noeuds)),
+    table_noeuds_sql, table_noeuds_sql_f(table_clean_noeuds)),
   tar_target(
-    table_cours_sql, table_cours_sql_f(table_cours))
+    table_cours_sql, table_cours_sql_f(table_clean_cours)),
+  tar_target(
+    requete_nb_collab, liens_par_etudiant_f(table_collab_sql)),
+  tar_target(
+    lien_par_paire_etudiant, lien_par_paire_etudiant_f(table_collab_sql)),
+  tar_target(
+    nb_etudiant, nb_etudiant_f(table_collab_sql)),
+  tar_target(
+    connectance, calcul_connectance(table_clean_noeuds, table_clean_collab)),
+  tar_target(
+    matrice_adjacence, matrice_adjacence_f(table_collab_sql)),
+  tar_target(
+    reseau, reseau_f(matrice_adjacence)),
+  tar_target(
+    figure_un, figure_un_f(reseau, matrice_adjacence)),
+  tar_target(
+    figure_deux, figure_deux_f(reseau, requete_nb_collab)),
+  tar_target(
+    figure_trois, figure_trois_f(reseau))
 )
-
-
-              
- 
 
     
 
